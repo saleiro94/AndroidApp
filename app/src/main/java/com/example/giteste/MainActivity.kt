bba61ade20d768daa227610de861cf2b5b7c8a3d
saleiro <saleiro94>
2021-03-13 10:33:49 +0000

    PÁgina incial

--- app/src/main/java/com/example/giteste/MainActivity.kt
+++ app/src/main/java/com/example/giteste/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.giteste
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun bnotas(view: View){
+        val button = findViewById<Button>(R.id.notas)
+        button.setOnClickListener{
+            val intent = Intent(this, Notas::class.java)
+            startActivity(intent)
+        }
     }
 }
